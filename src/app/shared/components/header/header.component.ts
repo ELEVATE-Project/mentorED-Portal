007a14f5cbee1e736fb589b55fe434f3a52bf019
Shanu0596 <113679271+Shanu0596@users.noreply.github.com>
2022-11-10 12:58:47 +0530

    Update header.component.ts

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -13,15 +13,6 @@ export class HeaderComponent implements OnInit {
     
   }
 
-  //  let btn1 = document.querySelector('#1');
-  //  let btn2 = document.querySelector('#2');
-  //  let btn3 = document.querySelector('#3');
-  //  let btn4 = document.querySelector('#4');
-
-  //  btn1.addEventListener('click',() => btn1.style.backgroundColor='maroon'  )
-  //  btn2.addEventListener('click',() => btn2.style.backgroundColor='')
-  //  btn3.addEventListener('click',() => btn3.style.backgroundColor='')
-  //  btn4.addEventListener('click',() => btn4.style.backgroundColor='')
 
   ngOnInit(): void {
 
